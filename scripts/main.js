dee752bfdc94e23f8d7a972fa9fbd992e8c019f2
kodosuke <ajaykrishnaanandhan@yahoo.com>
2022-05-05 14:47:40 +0530

    Added passwordLength as a parameter to generatePassword.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log(new Date())
-})
+const generatePassword = (passwordLength) => {
+
+    const lowerCases = "abcdefghjkmnpqrstuvwxyz";
+    const upperCases = "ABCDEFGHJKLMNPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const specialCases = "!#$%&*+-=?@^_{}[]()\/'\"`~,;:.<>\\";
+
+    let includedChars = [lowerCases, upperCases, numbers, specialCases];
+    let passwordChars = new Array();
+
+    for (let i = 0; i < passwordLength; i++) {
+
+        let chars = includedChars[Math.floor(Math.random() * 4)];
+        let index = Math.floor(Math.random() * chars.length);
+        passwordChars.push(chars[index]);
+    }
+
+    console.log(passwordChars.join(''));
+
+}
+
+
+document.addEventListener("DOMContentLoaded", () => generatePassword(16))
